9a528e35be05fe76c9dadd7b0d30e063387070dc
Sean YOUNG <syoung@e4r5p3.wethinkcode.co.za>
2017-11-14 10:21:01 +0200

    first

---
+++ include/header.h
@@ -0,0 +1,8 @@
+//
+// Created by Sean YOUNG on 2017/11/14.
+//
+
+#ifndef NPUZZLE_HEADER_H
+#define NPUZZLE_HEADER_H
+
+#endif //NPUZZLE_HEADER_H
---
+++ main.c
@@ -0,0 +1,4 @@
+//
+// Created by Sean YOUNG on 2017/11/14.
+//
+
