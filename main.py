c46ee73a8702e95bcbb8604e80d2e872a7598c7c
Sean YOUNG <syoung@e4r4p3.wethinkcode.co.za>
2017-11-20 12:40:29 +0200

    solvabled

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python
+
+import re
+
+
 class Node:
     i = 0
     h = 0
     n = 0
     grid = []
+    puzzle = []
 
     def manhattan_distance(self):
         print(self.grid)
@@ -22,18 +28,60 @@ class Node:
             for line in f:
                 line = line.partition('#')[0]
                 line = line.rstrip()
+                line = line.lstrip()
+                line = re.sub(' +', ' ', line)
                 line.split(" ")
                 puzzle.append(line)
+        if puzzle[0] == '':
+            puzzle.pop(0)
         self.n = int(puzzle[0])
         puzzle.pop(0)
         puzzle = [x.strip().split(' ') for x in puzzle]
         for x in puzzle:
             self.grid.append(map(int, x))
 
+    def is_solvable(self):
+        temp = []
+        solvable = False
+        for i in self.grid:
+            for j in i:
+                temp.append(j)
+        print(temp)
+        inv_count = 0
+        i = 0
+        while i < self.n * self.n - 1:
+            j = i + 1
+            while j < self.n * self.n:
+                if temp[i] != 0 and temp[j] != 0 and temp[i] > temp[j]:
+                    inv_count += 1
+                j += 1
+            i += 1
+        print(inv_count)
+        x_pos = 0
+        i = self.n - 1
+        while i >= 0:
+            j = self.n - 1
+            while j >= 0:
+                if self.grid[i][j] == 0:
+                    x_pos = self.n - i
+            j += 1
+        i += 1
+        if self.n % 2 == 1:
+            if inv_count % 2 == 0
+                solvable = True
+        else:
+            if x_pos % 2 == 0:
+                if inv_count % 2 == 1:
+                    solvable = False
+            else:
+                if inv_count % 2 == 0:
+                    solvable = True
+        return solvable
+
+
 
 start = Node()
 end = Node()
 start.read_puzzle("puzzles/puzzle.txt")
-start.read_puzzle("puzzles/end.txt")
-print(start.grid)
-
+end.read_puzzle("puzzles/end.txt")
+start.is_solvable()
